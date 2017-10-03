8a7c346b5ace95bf240b66f83a918158bffc0fc5
zhsir123 <zhsir123@gmail.com>
2017-10-03 22:34:18 +0900

    input_process
    generate notes from midi file and save with (note, velocity, time),
    shuffle x, y data

--- midi_util.py
+++ midi_util.py
@@ -1,37 +1,68 @@
 from mido import MidiFile
 import mido
 import numpy as np
+from sklearn.utils import shuffle
 
 
-def extract_mido(mido_path):
-    time = 0
-    prev = 0
-    notes = list()
+class DataSet:
+    def __init__(self, seq_length):
+        self.seq_length = seq_length
+        
+        self.min_ = 0 
+        self.max_ = 0 
 
-    music = MidiFile(mido_path)
-    for msg in music:
-        time += msg.time
+        self.notes = list()
 
-        if not msg.is_meta and msg.type == 'note_on':
-            # note, velocity, time
-            note = msg.bytes()[1:] + [time-prev]
-            prev = time
-            notes.append(note)   
+    def generate_notes(self, midi_path):
+        music = MidiFile(midi_path)
+        
+        for msg in music:
+            if msg.time != 0:
+                note = list()
+                # note = note, velocity, time                
+                if not msg.is_meta and msg.type == 'note_on':
+                    note = msg.bytes()[1:] + [msg.time]
+                else:
+                    note = [0, 0, msg.time]
+                self.notes.append(note)
 
-    return notes
+    def pre_process_note(self):
+        notes = np.array(self.notes)
 
+        note = notes[:, 0]
+        velocity = notes[:, 1]
+        time = notes[:, 2]
 
-def pre_process_note(notes):
-    notes = np.array(notes)
-    note = notes[:, 0]
-    velocity = notes[:, 1]
-    time = notes[:, 2]
+        note     /= max(note)
+        velocity /= max(velocity)
+        time     /= max(time)
 
-    print(max(note))
-    print(max(velocity))
-    print(max(time))
+        # (len) * 3  =>  (len, 3)
+        notes = np.dstack((note, velocity, time))[0]
+        self.notes = notes
+
+    def get_feed_data(self):
+        x_data = list()
+        y_data = list()
+
+        for i in range(len(self.notes) - self.seq_length - 1):
+            x_data.append(self.notes[i: i + self.seq_length])
+            y_data.append(self.notes[i + self.seq_length])
+
+        x_data = np.array(x_data)
+        y_data = np.array(y_data)
+
+        x_data, y_data = shuffle(x_data, y_data, random_state=5)
+        return x_data, y_data
+
 
 if __name__ == '__main__':
     path = "test.mid"
-    notes = extract_mido(path)
-    pre_process_note(notes)
+    data = DataSet(15)
+    data.generate_notes(path)
+    data.pre_process_note()
+    
+    x, y = data.get_feed_data()
+
+    print(x.shape)
+    print(y.shape)
