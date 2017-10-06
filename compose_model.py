ca24868d992a821fa3fc1679f4fd9fa007ace493
zhsir123 <zhsir123@gmail.com>
2017-10-06 14:58:59 +0900

    file rename, adam -> rms

--- compose_model.py
+++ compose_model.py
@@ -30,17 +30,30 @@ class Composer:
         multi_cells = rnn.MultiRNNCell([_lstm_cell() for _ in range(self.rnn_size)], state_is_tuple=True)
         output, states_ = tf.nn.dynamic_rnn(multi_cells, self.X, dtype=tf.float32)
 
-        pred = tf.contrib.layers.fully_connected(output, self.output_size)
+        with tf.variable_scope("rnn"):
+            with tf.name_scope("fc1"):
+                fc1 = tf.contrib.layers.fully_connected(output, 1024)
+                fc1 = tf.contrib.layers.batch_norm(fc1,
+                                                   center=True, scale=True,
+                                                   is_training=self.is_training)
+                fc1 = tf.nn.relu(fc1, name='relu1')
 
-        pred = tf.contrib.layers.fully_connected(pred[:, -1], self.output_size)
-        self.pred = pred
+            with tf.name_scope("fc2"):
+                fc2 = tf.contrib.layers.fully_connected(fc1, 1024)
+                fc2 = tf.contrib.layers.batch_norm(fc2,
+                                                   center=True, scale=True,
+                                                   is_training=self.is_training)
+                fc2 = tf.nn.relu(fc2, name='relu1')
 
-        self.cost = tf.reduce_sum(tf.pow(self.pred - self.Y, 2))
-        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
-        self.optimizer = optimizer
+            pred = tf.contrib.layers.fully_connected(fc2[:, -1], self.output_size)
+            self.pred = pred
 
-        correct_pred = tf.equal(self.pred, self.Y)
-        self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+            self.cost = tf.losses.mean_squared_error(self.Y, self.pred)
+            optimizer = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
+            self.optimizer = optimizer
+
+            correct_pred = tf.equal(self.pred, self.Y)
+            self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     def train(self, x, y, is_training=True, keep_prob=0.2):
         return self.sess.run([self.optimizer, self.pred, self.accuracy],
