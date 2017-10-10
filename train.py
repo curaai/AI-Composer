771dca106cd7aad67df857a49d7de65a3bf72355
zhsir123 <zhsir123@gmail.com>
2017-10-10 16:22:20 +0900

    tensorboard, song path moving, fully connected layer 추가
    fully connected layer * 2
    all songs in the songs dir
    canon, flower_dance 추가

--- train.py
+++ train.py
@@ -15,9 +15,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(epilog=exmaple_text)
     parser.add_argument('-sl', '--seq_length', type=int, default=30, help='How many set sequence length to train')
     parser.add_argument('-b', '--batch_size', type=int, default=20, help="Batch size")
+    parser.add_argument('-sm', '--summary', default='board/composer', help='Tensor board path')
     parser.add_argument('-i', '--iteration', type=int, default=2000, help='Iteration')
     parser.add_argument('-s', '--save', default='save/save.ckpt', help='Save trained network')
-    parser.add_argument('-m', '--midi', default='test.mid', help="File to train")
+    parser.add_argument('-m', '--midi', default='songs/canon.mid,songs/flower_dance.mid', help="File to train")
     args = parser.parse_args()
 
     data = midi_util.DataSet(args.seq_length, args.batch_size)
@@ -29,11 +30,15 @@ if __name__ == '__main__':
     with tf.Session() as sess:
         composer = model.Composer(sess, args.seq_length)
         sess.run(tf.global_variables_initializer())
+        writer = tf.summary.FileWriter(args.summary, sess.graph)
         print("Learning Start !!!")
 
         for i in range(args.iteration):
             x, y = data.get_feed_data()
-            _, pred, accuracy, loss = composer.train(x, y)
+            _, pred, accuracy, loss, summary = composer.train(x, y)
+            if i % 10 == 0:
+                writer.add_summary(summary, float(i))
+
             if i % 50 == 0:
                 print("Iteration: " + str(i))
                 print("Loss: " + str(loss))
